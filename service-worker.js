244b517460d080e6de62acd501a8d36aefbb74fb
rifqiabrory <rifqi.upm@gmail.com>
2020-08-17 11:03:54 +0700

    web push notification

--- service-worker.js
+++ service-worker.js
@@ -91,3 +91,27 @@ self.addEventListener("activate", function (event) {
     })
   );
 });
+
+// Notification Actions
+self.addEventListener("notificationclick", function (event) {
+  // Tambahkan baris berikut
+  event.notification.close();
+  if (!event.action) {
+    // Penguna menyentuh area notifikasi diluar action
+    console.log("Notification Click.");
+    return;
+  }
+  switch (event.action) {
+    case "yes-action":
+      console.log("Pengguna memilih action yes.");
+      // buka tab baru
+      clients.openWindow("https://google.com");
+      break;
+    case "no-action":
+      console.log("Pengguna memilih action no");
+      break;
+    default:
+      console.log(`Action yang dipilih tidak dikenal: '${event.action}'`);
+      break;
+  }
+});
